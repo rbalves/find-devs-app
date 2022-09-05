c180ae397e25e2291c083baaf69897dafcaa1412
Rafael Alves <rbalves.ads@gmail.com>
2022-09-05 10:09:28 -0300

    feat: add library NativeBase

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
+
+import { Center, NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center safeArea height="100%">
+        <Text style={{ fontWeight: "700", fontSize: 32, lineHeight: 40 }}>
+          Find Devs App
+        </Text>
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
