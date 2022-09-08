94241b48a6f1a3a526d5c99b8c79faa6fd2b9731
Rafael Alves <rbalves.ads@gmail.com>
2022-09-08 20:46:16 -0300

    chore: install and configure react-redux

--- App.tsx
+++ App.tsx
@@ -4,15 +4,20 @@ import { Center, NativeBaseProvider } from "native-base";
 
 import { NavigationContainer } from "@react-navigation/native";
 
+import { Provider } from "react-redux";
+import store from "./src/store";
+
 export default function App() {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
-        <Center safeArea height="100%">
-          <Text style={{ fontWeight: "700", fontSize: 32, lineHeight: 40 }}>
-            Find Devs App
-          </Text>
-        </Center>
+        <Provider store={store}>
+          <Center safeArea height="100%">
+            <Text style={{ fontWeight: "700", fontSize: 32, lineHeight: 40 }}>
+              Find Devs App
+            </Text>
+          </Center>
+        </Provider>
       </NativeBaseProvider>
     </NavigationContainer>
   );
