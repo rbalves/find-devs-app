c5f4196b1161fb89a3e52c9debbfc8711fac3623
Rafael Alves <rbalves.ads@gmail.com>
2022-09-08 21:11:56 -0300

    feat: create developers strucuture

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import { categories, favorites, stacks } from "./modules";
+import { categories, favorites, stacks, developers } from "./modules";
 
 const store = configureStore({
   reducer: {
     categories,
     favorites,
     stacks,
+    developers,
   },
 });
 
--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,5 +1,6 @@
 import favorites from "./favorites/reducer";
 import categories from "./categories/reducer";
 import stacks from "./stacks/reducer";
+import developers from "./developers/reducer";
 
-export { favorites, categories, stacks };
+export { favorites, categories, stacks, developers };
