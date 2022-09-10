35ce788c3a1e8c442ef42c072f4f96e04007c0f5
ajuniordx <alexjunioferreira17@gmail.com>
2022-09-10 03:58:23 -0300

    resolver bugInMerge

--- App.tsx
+++ App.tsx
@@ -1,34 +1,26 @@
-import { NativeBaseProvider } from "native-base";
+import { Text } from "react-native";
+import { Center, NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
+import store from "./src/store";
 
 //Import Views
 import Home from "./src/screens/Home";
-import Search from "./src/screens/Search";
+import Search from "./src/screens/search";
 
 const Views = createNativeStackNavigator();
-
-import { Provider } from "react-redux";
-import store from "./src/store";
 
 export default function App() {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
-<<<<<<< HEAD
-        <Views.Navigator>
-          <Views.Screen name="Home" component={Home} />
-          <Views.Screen name="Buscar" component={Search} />
-        </Views.Navigator>
-=======
         <Provider store={store}>
-          <Center safeArea height="100%">
-            <Text style={{ fontWeight: "700", fontSize: 32, lineHeight: 40 }}>
-              Find Devs App
-            </Text>
-          </Center>
+          <Views.Navigator>
+            <Views.Screen name="Home" component={Home} />
+            <Views.Screen name="Buscar" component={Search} />
+          </Views.Navigator>
         </Provider>
->>>>>>> main
       </NativeBaseProvider>
     </NavigationContainer>
   );
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,13 @@
-import { Button, Text } from "react-native";
+import { Button } from "react-native";
 import { Center, View } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 
-export default function App() {
+export default function Home() {
   const navigation = useNavigation();
 
   return (
     <View>
       <Center safeArea height="100%">
-        <Text style={{ fontWeight: "700", fontSize: 32, lineHeight: 40 }}>
-          HOME | Find Devs App
-        </Text>
         <Button title="Buscar" onPress={() => navigation.navigate("Buscar")} />
       </Center>
     </View>
