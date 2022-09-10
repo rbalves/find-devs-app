f14741d7109a188c962aac1c30ec9b94161df99f
ajuniordx <alexjunioferreira17@gmail.com>
2022-09-10 01:09:50 -0300

    init layout screen Search

--- App.tsx
+++ App.tsx
@@ -3,16 +3,20 @@ import { Text } from "react-native";
 import { Center, NativeBaseProvider } from "native-base";
 
 import { NavigationContainer } from "@react-navigation/native";
+import Search from "./src/screens/search";
 
 export default function App() {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
+        {/*
         <Center safeArea height="100%">
           <Text style={{ fontWeight: "700", fontSize: 32, lineHeight: 40 }}>
             Find Devs App
           </Text>
         </Center>
+  */}
+        <Search />
       </NativeBaseProvider>
     </NavigationContainer>
   );
---
+++ src/screens/search/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "native-base";
+
+export default function Search() {
+  return (
+    <View>
+      <Text>
+        <p>Hello</p>
+      </Text>
+    </View>
+  );
+}
